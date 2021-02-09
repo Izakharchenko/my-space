1026c7bf1b43fa7b5000123266a3ccd4a9be86af
Ivan Zakharchenko <zakharchenko.i.p@yandex.ua>
2021-02-09 15:21:48 +0200

    add index view for users

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -15,6 +15,11 @@
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
+                    <x-nav-link
+                        :href="route('admin.user.index')"
+                        :active="request()->routeIs('amin.user.index')">
+                        {{ __('Users') }}
+                    </x-nav-link>
                 </div>
             </div>
 
@@ -64,7 +69,10 @@
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
             <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                {{ __('Dashboard') }}
+                {{ __('Dashboard') }}1
+            </x-responsive-nav-link>
+            <x-responsive-nav-link :href="route('admin.user.index')" :active="request()->routeIs('admin.user.index')">
+                User
             </x-responsive-nav-link>
         </div>
 
