af09b3b04f5ff1175684896d4467bbf14e28aafd
Ivan Zakharchenko <zakharchenko.i.p@yandex.ua>
2021-02-09 14:30:44 +0200

    Add models role & user relationship

--- app/Models/User.php
+++ app/Models/User.php
@@ -40,4 +40,12 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    /**
+     * Get many the roles for the user.
+     */
+    public function roles()
+    {
+        return $this->belongsToMany(Role::class);
+    }
 }
